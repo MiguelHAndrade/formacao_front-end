bacaa61800d412ee019711e3474f806af2019663
Miguel Andrade <101271954+MiguelHAndrade@users.noreply.github.com>
2024-02-02 13:28:50 +0000

    Lesson 216

--- formacao_front-end/JAVASCRIPT-CURSO/02-OPERADORES/js/script.js
+++ formacao_front-end/JAVASCRIPT-CURSO/02-OPERADORES/js/script.js
@@ -3,7 +3,7 @@ console.log(typeof 2)
 console.log(typeof 5.12)
 console.log(typeof -23)
 
-console.log('//////////')
+console.log('\n////////// ////////// //////////\n')
 
 // 2 - Operações aritméticas
 console.log(2 + 3)
@@ -14,7 +14,7 @@ console.log(10 * 5)
 console.log(2 * (10 - 5))
 console.log(2 * 10 - 5)
 
-console.log('//////////')
+console.log('\n////////// ////////// //////////\n')
 
 // 3 - special numbers 
 console.log(typeof Infinity)
@@ -23,7 +23,7 @@ console.log(typeof 5 * '3')
 console.log(5 * 'b')
 console.log(typeof NaN)
 
-console.log('//////////')
+console.log('\n////////// ////////// //////////\n')
 
 // 4 - Strings
 console.log("Um texto")
@@ -33,3 +33,60 @@ console.log('Ainda mais um texto')
 console.log(typeof "Um texto")
 console.log(typeof `Mais um texto`)
 console.log(typeof 'Ainda mais um texto')
+
+console.log('\n////////// ////////// //////////\n')
+
+// 5 - Símbolos especiais em strings
+console.log('Texto \nem duas linhas')
+console.log('Expaço \nde  tab')
+
+console.log('\n////////// ////////// //////////\n')
+
+// 6 - Concatenação
+console.log('Olá!' + ' Está' + ' tudo' + ' bem?')
+
+console.log('\n////////// ////////// //////////\n')
+
+// 7 - Interpolação (template strings)
+console.log(`A soma de 2 + 2 é: ${2 + 2}`)
+
+console.log('\n////////// ////////// //////////\n')
+
+// 8 - Booleans
+console.log(true)
+
+console.log(5 > 20)
+
+console.log(30 > 20)
+
+console.log(typeof false)
+
+console.log('\n////////// ////////// //////////\n')
+
+// 9 - Comparações
+console.log(5 <= 5)
+console.log(5 < 5)
+console.log(10 == 10)
+console.log(10 == 9)
+console.log(10 != 9)
+
+console.log('\n////////// ////////// //////////\n')
+
+// 10 - Comparação de indêntico
+console.log(9 == '9')
+console.log(9 + '9')
+console.log(9 === '9')
+console.log(9 != '9')
+console.log(9 !== '9')
+
+console.log('\n////////// ////////// //////////\n')
+
+// 11 - Operadores lógicos
+console.log(true && true)
+console.log(true && false)
+console.log(5 > 2 && 2 < 10)
+console.log(5 > 2 && 'João' === 1)
+console.log(5 > 2 || 'João' === 1)
+console.log(5 > 50 || 5 > 100)
+console.log(!true)
+console.log(!5 > 2)
